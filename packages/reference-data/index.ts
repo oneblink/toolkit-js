d2466f7f64a99863f76573a76118a68dbc53cc21
David Porter <david@oneblink.io>
2022-07-11 15:06:01 +1000

    ON-23284 # add reference data class

---
+++ packages/reference-data/index.ts
@@ -0,0 +1,55 @@
+interface ReferenceDataMeta {
+  type: string;
+  hash: string;
+}
+
+type ReferenceDataType<T> = T extends Array<infer R> ? R : T;
+
+interface ReferenceData<T = unknown[]> extends ReferenceDataMeta {
+  value: Array<ReferenceDataType<T>>;
+}
+
+export class ReferenceDataStore {
+  store: LocalForage;
+
+  constructor(store: LocalForage) {
+    this.store = store;
+  }
+
+  async emptyStore() {
+    await this.store.clear();
+  }
+
+  async updateStore<T>(referenceData: ReferenceData[]) {
+    await this.store.clear();
+    for (const data of referenceData) {
+      await this.store.setItem(data.type, data);
+    }
+  }
+
+  async getReferenceDataByType<T>(
+    typeKey: string
+  ): Promise<ReferenceData<T> | null> {
+    return await this.store.getItem(typeKey);
+  }
+
+  async getAllReferenceData() {
+    const referenceData: Record<string, Array<unknown>> = {};
+    await this.store.iterate((record: ReferenceData) => {
+      if (record) {
+        referenceData[record.type] = record.value;
+      }
+    });
+    return referenceData;
+  }
+
+  async getAllReferenceDataMeta(store: LocalForage) {
+    const referenceDataMeta: Record<string, unknown> = {};
+    await store.iterate((record: ReferenceData) => {
+      if (record) {
+        referenceDataMeta[record.type] = record.hash;
+      }
+    });
+    return referenceDataMeta;
+  }
+}
