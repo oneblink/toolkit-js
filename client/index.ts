fd0c77a534206a00604510937d3c9402dc4e11d4
David Porter <david@oneblink.io>
2022-07-12 09:26:56 +1000

    ON-23284 # move files around export services

--- client/index.ts
+++ client/index.ts
@@ -1,1 +1,5 @@
-export { ReferenceDataStore } from "./reference-data";
+import validateConfig from '../packages/validate-config'
+
+export { ReferenceDataStore } from './src/reference-data'
+export * as definitionService from './src/definition-manipulation'
+export { validateConfig }
